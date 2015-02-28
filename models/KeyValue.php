a63013432a1dddf242d3200c8a24abe6a223aad0
Marcin Wiatr <marcin@qi-interactive.com>
2015-02-28 18:07:12 +0000

    Added findByKey and EVENT_KEY_NOT_FOUND

--- models/KeyValue.php
+++ models/KeyValue.php
@@ -3,6 +3,8 @@
 namespace mata\keyvalue\models;
 
 use Yii;
+use yii\base\Event;
+use mata\base\MessageEvent;
 
 /**
  * This is the model class for table "mata_keyvalue".
@@ -11,6 +13,9 @@ use Yii;
  * @property string $Value
  */
 class KeyValue extends \mata\db\ActiveRecord {
+
+    const EVENT_KEY_NOT_FOUND = "event_key_not_found";
+
     /**
      * @inheritdoc
      */
@@ -38,5 +43,14 @@ class KeyValue extends \mata\db\ActiveRecord {
         'Key' => 'Key',
         'Value' => 'Value',
         ];
+    }
+
+    public static function findByKey($key) {
+        $model = self::find()->where(["Key" => $key])->one();
+
+        if ($model == null)
+            Event::trigger(self::className(), self::EVENT_KEY_NOT_FOUND, new MessageEvent($key));
+
+        return $model != null ? $model->Value : null;
     }
 }
